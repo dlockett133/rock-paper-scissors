06a66c412caf1cb4174aab8d9ab5c40519008078
Daikeen Lockett <daikeen_lockett@yahoo.com>
2021-10-11 01:41:59 -0400

    Created algorithm for Rock, Paper, Scissors game

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-const cpuResult = ["Rock", "Paper", "Scissors"];
+const cpuResult = ["ROCK", "PAPER", "SCISSORS"];
+let userPlay;
 
 let computerPlay = () => {
+    i = cpuResult.length;
+    i = Math.floor(Math.random() * i);
+    return (cpuResult[i]);
 
+}
+
+let cpuHand = computerPlay();
+
+let gameOver = false;
+
+while(gameOver == false){
+    do {
+
+        userPlay = prompt("Rock, Paper Scissors: ").toUpperCase();
+        if (cpuResult.includes(userPlay) == false ){
+    
+            console.log("Please Choose Rock, Paper or Scissors!!")
+            
+        }
+    }
+    while (cpuResult.includes(userPlay) == false );
+
+    console.log(`You chose ${userPlay}!`);
+    console.log(`The CPU chose ${cpuHand}!`);
+
+    if (userPlay === cpuHand){
+
+        console.log("Draw! Try Again!");
+
+    } else if ((userPlay === "ROCK" && cpuHand === "SCISSORS") || 
+        (userPlay === "PAPER" && cpuHand === "ROCK") ||
+        (userPlay === "SCISSORS" && cpuHand === "PAPER")){
+
+        console.log("You Win!!!");
+        gameOver = true;
+
+    } else {
+
+        console.log("You Lose! Try Again!");
+    }
 }
